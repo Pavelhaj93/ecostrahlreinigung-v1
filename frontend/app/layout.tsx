46a0242578fe95d38b86583dfdcacc357a29c4c3
Pavelhaj93 <pavelhajduch93@gmail.com>
2025-11-15 19:30:08 +0700

    feat: fix visual editing

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -6,6 +6,11 @@ import Header from '@/app/components/Header'
 import {industry} from './fonts'
 import FooterSection from './components/FooterSection'
 import {SanityLive} from '@/sanity/lib/live'
+import {VisualEditing} from 'next-sanity'
+import DraftModeToast from './components/DraftModeToast'
+import {draftMode} from 'next/headers'
+import {Toaster} from 'sonner'
+import {handleError} from './client-utils'
 
 export const metadata: Metadata = {
   title: 'Ecostrahlreinigung - Mobile Reinigungsservices in Bayern',
@@ -21,7 +26,9 @@ export const metadata: Metadata = {
   ],
 }
 
-export default function RootLayout({children}: {children: React.ReactNode}) {
+export default async function RootLayout({children}: {children: React.ReactNode}) {
+  const {isEnabled: isDraftMode} = await draftMode()
+
   return (
     <html lang="de" className={`${industry.variable} bg-white text-black`}>
       <body className={industry.className}>
@@ -29,7 +36,16 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
         <main>{children}</main>
         <FooterSection />
         <SpeedInsights />
-        <SanityLive />
+        <Toaster />
+        {isDraftMode && (
+          <>
+            <DraftModeToast />
+            {/*  Enable Visual Editing, only to be rendered when Draft Mode is enabled */}
+            <VisualEditing />
+          </>
+        )}
+        {/* The <SanityLive> component is responsible for making all sanityFetch calls in your application live, so should always be rendered. */}
+        <SanityLive onError={handleError} />
       </body>
     </html>
   )
